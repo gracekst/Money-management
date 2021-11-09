96f46f7b4df0c2118556d531ce682ae362123b28
Pantila Kosalaraksa <pantila2547@gmail.com>
2021-11-09 19:15:54 +0700

    add code which will calculate days and  save per days or weeks version1

--- MoneyPrj.c
+++ MoneyPrj.c
@@ -2,7 +2,8 @@
 #include<stdlib.h>
 #include<string.h>
 #include<stdbool.h>
-
+#include <time.h>
+#include <math.h>
 
 // necessay information
 struct date
@@ -26,11 +27,73 @@ struct item_info
     int price;
 };
 
+//find days between two dates
+int day_between_date_today(int day1, int month1, int year1, int day2, int month2 ,int year2)
+{
+    //struct tm range of tm_mon = 0-11
+    --month1;
+    --month2;
+
+    //year in struct tm is count from 1900 | for 1901 tm_year = 1
+    year1 -= 1900;
+    year2 -= 1900;
+
+    time_t day1_t, day2_t;
+    struct tm day1_date,day2_date;
+
+    //assign time
+    time(&day1_t);
+    time(&day2_t);
+
+    //assign time to struct
+    day1_date = *localtime(&day1_t);
+    day2_date = *localtime(&day2_t);
+
+    //change date month and year
+    day1_date.tm_mday = day1;
+    day1_date.tm_mon = month1;
+    day1_date.tm_year = year1;
+
+    day2_date.tm_mday = day2;
+    day2_date.tm_mon = month2;
+    day2_date.tm_year = year2;
+
+    //change information in struct to sec 
+    day1_t = mktime(&day1_date);
+    day2_t = mktime(&day2_date);
+
+    //find different sec and day between two dates
+    int diff_sec, diff_day;
+    diff_sec = difftime(day2_t,day1_t);
+    diff_day = diff_sec / (60*60*24);
+
+    //return amount of days between two dates
+    return diff_day;
+}
+
+//money to save per day
+double daily_save(int days, double money)
+{
+    double save_pday;
+
+    //calculate amount per day
+    save_pday = money / days;
+
+    return save_pday;
+}
+
+//money to save per week
+double weekly_save(int week, double money)
+{
+    double save_pweek;
+
+    save_pweek = money / week;
+    return save_pweek;
+}
     
 int main()
 {   
     bool week = false;
-    bool day = false;
 
     char save_type[50];
 
@@ -109,27 +172,152 @@ int main()
         total_price = total_price + itm_info.price;
     }
 
-    printf("The total price for all items: %d", total_price);
+    printf("The total price for all items: %d\n", total_price);
     fprintf(fptr,"The total price for all itmes: %d\n", total_price);
 
-    printf("How would you like to save? (type week/day): ");
-    scanf("%s", &save_type);
-
-    if(save_type == "week")
+    while(1)
     {
-        //code
+        printf("How would you like to save? (type week/day): ");
+        scanf("%s", &save_type);
+
+        if(strcmp(save_type,"week") == 0)
+        {
+            week = true;
+            break;
+        }
+        else if (strcmp(save_type,"day") == 0)
+        {
+            week = false;
+            break;
+        }
+        else
+        {
+            printf("Erro, please try agian.\n");
+        }
     }
 
-    else if (save_type == "day")
+    time_t today;
+    time(&today);
+    struct tm today_s;
+    today_s = *localtime(&today);
+    int todayMonth = today_s.tm_mon + 1;
+    int todayYear = today_s.tm_year + 1900;
+
+    int days = day_between_date_today(today_s.tm_mday, todayMonth, todayYear, dt.day, dt.month, dt.year);
+
+    int m,f,s;
+    if(todayYear == dt.year)
     {
-        //code
+        m = dt.month - todayMonth;
     }
-    
     else
     {
-        // code
+        f = (12 - todayMonth) + 1;
+        s = dt.month - 1;
+        m = f+s;
     }
 
+    int dayEachMonth[m];
+
+    if(todayYear == dt.year)
+    {
+        int x = 0;
+        for(int i = todayMonth; i < dt.month; i++)
+        {
+            dayEachMonth[x++] = day_between_date_today(1,i,todayYear,1,i+1,dt.year);
+        }
+    }
+    else
+    {
+        int i = 0;
+        for(int x = 0; x < f-1; x++)
+        {
+            dayEachMonth[i++] = day_between_date_today(1,todayMonth++,todayYear,1,todayMonth+1,todayYear); 
+        }
+        dayEachMonth[i++] = day_between_date_today(1,todayMonth,todayYear,1,1,dt.year);
+        int jan = 1;
+        for(int x = 0; x < s; x++)
+        {
+            dayEachMonth[i++] = day_between_date_today(1,jan++,dt.year,1,jan+1,dt.year);
+        }
+    }
+
+    double price_type[2];
+    double price = total_price;
+    int howManyMonth = 0;
+    int aMonth = m;
+    int dDay = days;
+    int x = 0;
+    while(price > 0)
+    {
+        if(price > ext_money && dDay > dayEachMonth[x])
+        {
+            price_type[0] = ext_money;
+            price -= ext_money;
+            dDay -= dayEachMonth[x++];
+            howManyMonth++;
+            aMonth--;
+        }
+        else
+        {
+            price_type[1] = price;
+            break;
+        }
+    }
+
+    double save[2];
+    int dayLeft = days;
+    int weekLeft;
+    int totalday = 0;
+    int totalweek;
+
+    if(aMonth > 0)
+    {
+        for(int j = 0; j < howManyMonth; j++)
+        {
+            dayLeft -= dayEachMonth[j];
+            totalday += dayEachMonth[j];
+        }
+
+        if(week == true)
+        {
+            totalweek = totalday / 7;
+            weekLeft = (dayLeft + (totalday % 7)) / 7;
+
+            save[0] = weekly_save(totalweek, price_type[0]) ;
+            save[1] = weekly_save(weekLeft, price_type[1]);
+        }
+        else
+        {
+            save[0] = daily_save(totalday, price_type[0]);
+            save[1] = daily_save(dayLeft, price_type[1]);
+        }
+    }
+    else if(aMonth == 0 && days == dayEachMonth[0])
+    {
+        if(week == true)
+        {
+            totalweek = days / 7;
+
+            save[0] = weekly_save(totalweek, price_type[0]) ;
+        }
+        else
+        {
+            save[0] = daily_save(days, price_type[0]);
+        }
+    }
+    else
+    {
+        printf("Invalid date");
+    }
+
+    printf("%lf %lf %lf %d %d %d %d\n",price_type[0], price_type[1],price,howManyMonth,aMonth,dDay,x);
+    printf("%lf %lf %d %d %d %d\n",save[0],save[1],dayLeft,weekLeft,totalday,totalweek);
+    printf("%d %d\n ",days, m);
+    for(int i = 0; i < m; i++)
+    {
+        printf("%d ", dayEachMonth[i]);
+    }
+    
     return 0;
 }
-    
